832598b5f676918f3aa81cafe71a97b9de5151d8
AneeqBass <aneeqbass@gmail.com>
2024-07-01 13:54:40 +0200

    "Added TestimonialsView.vue with carousel component and testimonials content"

---
+++ src/views/TestimonialsView.vue
@@ -0,0 +1,84 @@
+@<template>
+  <div class="bgContainerTest lazy">
+        <img class="menuIcon fixed-top" src="https://aneeqbass.github.io/hostedImages_portfolio/images/menuIcon.png" alt="menuIcon" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
+
+        <div class="container-fluid text-center">
+
+            <div class="container text-center">
+                <div class="row row-cols-1 vw-75 vh-100 min-vw-75 min-vh-100">
+                  <div class="col">
+                    <div id="carouselExampleCaptions" class="carousel slide vh-100 with-blur-backdrop">
+                
+                        <div class="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 3"></button>
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                          <div id="contBottom" class="carousel-item active vh-100">
+                            <img id="testiPic" src="https://aneeqbass.github.io/hostedImages_portfolio/images/mahmud.png" class="vh-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                              <h1 id="title">Mahmud's Testimonial</h1>
+                              <p class="fs-4">"Aneeq's unwavering dedication to excellence and his collaborative spirit makes him indispensable in every project he undertakes. Aneeq is a valuable asset to any team .He works well in fast paced environments and constantly delivering brilliant results."</p>
+                            </div>
+                          </div>
+                          <div class="carousel-item">
+                            <img id="testiPic" src="https://aneeqbass.github.io/hostedImages_portfolio/images/zubayr.png" class="vh-100" alt="...">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h1 id="title">Zubayr's Testimonial</h1>
+                                <p class="fs-4">"Aneeq is a dedicated professional known for his attention to detail and commitment to excellence. With strong communication skills and a passion for problem-solving, he consistently drives results and ensures project success. A valuable asset to any team, Aneeq's collaborative nature and innovative thinking make him a trusted partner in achieving goals."</p>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                            <img id="testiPic" src="https://aneeqbass.github.io/hostedImages_portfolio/images/ockers.png" class="vh-100" alt="...">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h1 id="title">Caleb's Testimonial</h1>
+                                <p class="fs-4">"I am grateful to call Aneeq a close friend. He has great work ethic, analytical and problem solving skills. He thrives in collaborative settings as well as when he works on his own. He is kind, generous and hilarious. Aneeq would be a valuable asset to any organization."</p>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                            <img id="testiPic" src="https://aneeqbass.github.io/hostedImages_portfolio/images/mohammed.png" class="vh-100" alt="...">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h1 id="title">Mohammed's Testimonial</h1>
+                                <p class="fs-4">"Aneeq embodies excellence with his meticulous attention to detail and collaborative spirit. His innovative approach and dedication consistently yield exceptional results. Working alongside Aneeq ensures not just success, but surpassing expectations."</p>
+                              </div>
+                          </div>
+                          <div class="carousel-item">
+                            <img id="testiPic" src="https://aneeqbass.github.io/hostedImages_portfolio/images/matthew.png" class="vh-100" alt="...">
+                              <div class="carousel-caption d-none d-md-block">
+                                <h1 id="title">Matthew's Testimonial</h1>
+                                <p class="fs-4">"Aneeq's blend of precision and collaboration consistently delivers exceptional results, redefining success at every turn."</p>
+                              </div>
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            
+            
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
