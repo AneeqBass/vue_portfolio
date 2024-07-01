b5a080b1880175b3d29082f63d853158281a59fd
AneeqBass <aneeqbass@gmail.com>
2024-07-01 12:13:58 +0200

    Navbar Component Added; Landing page changed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,287 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBarComp/>
   <router-view/>
 </template>
-
+<script>
+import NavBarComp from './components/NavBar.vue'
+export default{
+  components:{
+    NavBarComp
+  }
+}
+</script>
 <style>
+@import url('https://fonts.cdnfonts.com/css/commando?styles=7971');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Commando', sans-serif;
+    -webkit-text-fill-color: white; 
+    -webkit-text-stroke: 1px #060018;
+}
+#title{
+    -webkit-text-fill-color:white;
+    -webkit-text-stroke: 2px #060018;
+    -webkit-animation: glow 1s ease-in-out infinite alternate;
+    -moz-animation: glow 1s ease-in-out infinite alternate;
+    animation: glow 1s ease-in-out infinite alternate;
+}
+#titleAlt{
+    -webkit-text-fill-color:white;
+    -webkit-text-stroke: 2px #060018;
+    -webkit-animation: glow 1s ease-in-out infinite alternate;
+    -moz-animation: glowAlt 1s ease-in-out infinite alternate;
+    animation: glowAlt 1s ease-in-out infinite alternate;
+}
+#glow {
+    -webkit-animation: iglow 1s ease-in-out infinite alternate;
+    -moz-animation: iglow 1s ease-in-out infinite alternate;
+    animation: iglow 1s ease-in-out infinite alternate;
 }
 
-nav {
-  padding: 30px;
+@keyframes iglow {
+  from {
+    box-shadow: 0 0 0 #fff, 0 0 5px #fff, 0 0 10px #df0056, 0 0 15px #df0056, 0 0 20px #df0056, 0 0 25px #df0056, 0 0 30px #e60073;
+  }
+  to {
+    box-shadow: 0 0 5px #fff, 0 0 10px #ff4da6, 0 0 15px #ff4da6, 0 0 20px #ff4da6, 0 0 25px #ff4da6, 0 0 30px #ff4da6, 0 0 35px #ff4da6;
+  }
+}
+@keyframes glow {
+  from {
+    text-shadow: 0 0 0 #fff, 0 0 5px #fff, 0 0 10px #df0056, 0 0 15px #df0056, 0 0 20px #df0056, 0 0 25px #df0056, 0 0 30px #e60073;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #ff4da6, 0 0 15px #ff4da6, 0 0 20px #ff4da6, 0 0 25px #ff4da6, 0 0 30px #ff4da6, 0 0 35px #ff4da6;
+  }
+}
+@keyframes glowAlt {
+  from {
+    text-shadow: 0 0 0 #fff, 0 0 5px #fff, 0 0 10px #031663, 0 0 15px #031663, 0 0 20px #031663, 0 0 25px #031663, 0 0 30px #031663;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #5152cc, 0 0 15px #5152cc, 0 0 20px #5152cc, 0 0 25px #5152cc, 0 0 30px #5152cc, 0 0 35px #5152cc;
+  }
+}
+body, html {
+  background-color: #0c0c0c;
+}
+.bgContainerInd {
+  background-image: url("https://aneeqbass.github.io/hostedImages_portfolio/images/oasisBgImg.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;  
+  background-attachment: fixed;
+  height: 100%;
+  }
+.bgContainerAb {
+  background-image: url("https://aneeqbass.github.io/hostedImages_portfolio/images/loungeBgImg.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;  
+  
+  background-attachment: fixed;
+}
+.bgContainerProj {
+  background-image: url("https://aneeqbass.github.io/hostedImages_portfolio/images/desktopBgImg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;  
+  background-attachment: fixed;
+  height: 100%;
+}
+.bgContainerTest {
+  background-image: url("https://aneeqbass.github.io/hostedImages_portfolio/images/buildingsBgImg.jpeg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;  
+  background-attachment: fixed;
+  height: 100%;
+}
+.bgContainerCont {
+  background-image: url("https://aneeqbass.github.io/hostedImages_portfolio/images/sunsetBgImg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;  
+  background-attachment: fixed;
+  height: 100%;
+}
+.bgContainerAbBlur {
+  background-image: url("https://aneeqbass.github.io/hostedImages_portfolio/images/loungeBgImg.png");
+  background-position: center;
+  transform: rotate(180deg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+}
+.with-blur-backdrop {
+  background-color: rgba(0, 0, 0, 0);
+  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(5px);
+  height: inherit;
+}
+footer{
+  background-color: #0c0c0c;
+  border-color: transparent;
+  position: absolute;
+}
+.menuIcon{
+  height: 70px;
+  margin-left: 15.5px;
+  margin-top: 15.5px;
+  -webkit-filter: drop-shadow(2px 2px 0 #e21861)
+                  drop-shadow(-2px -2px 0 #e21861);
+  filter: drop-shadow(2px 2px 0 #e21861) 
+          drop-shadow(-2px -2px 0 #e21861);
+  -webkit-animation: aboutGlow 1s ease-in-out infinite alternate;
+  -moz-animation: aboutGlow 1s ease-in-out infinite alternate;
+  animation: aboutGlow 1s ease-in-out infinite alternate;   
+  -webkit-filter:aboutGlow ease-in-out infinite alternate; 
+  }
+  @keyframes menuIcon{
+    
+  }
+.menuIcon:hover{
+  background-color: rgb(246, 0, 180, 0.90) ;
+}
+.menuIcon2{
+  height: 70px;
+  -webkit-filter: drop-shadow(2px 2px 0 #e21861)
+                  drop-shadow(-2px -2px 0 #e21861);
+  filter: drop-shadow(2px 2px 0 #e21861) 
+          drop-shadow(-2px -2px 0 #e21861);
+  -webkit-animation: aboutGlow 1s ease-in-out infinite alternate;
+  -moz-animation: aboutGlow 1s ease-in-out infinite alternate;
+  animation: aboutGlow 1s ease-in-out infinite alternate;   
+  -webkit-filter:aboutGlow ease-in-out infinite alternate; 
+}
+.menuIcon2:hover{
+  background-color: rgb(246, 0, 180, 0.90) ;
+}
+.portrait{
+  padding-top: 7%;
+}
+.paddingCont{
+  padding-left: 250px;
+  padding-right: 250px;
+}
+#pageSelected{
+  background-color: rgb(246, 0, 180, 0.66) ;
+}
+#pageSelected:hover{
+  background-color: rgb(246, 0, 180, 0.90) ;
+}
+li:hover{
+  background-color: rgb(246, 0, 180, 0.90) ;
+}
+.sidebar li{
+  padding-bottom: 20px;
+  padding-left: 20px;
+  list-style: none;
+}
+.sidebar a{
+  padding-bottom: 20px;
+}
+#aboutMePic{
+  -webkit-filter: drop-shadow(2px 2px 0 #e21861)
+                  drop-shadow(-2px -2px 0 #e21861);
+  filter: drop-shadow(2px 2px 0 #e21861) 
+          drop-shadow(-2px -2px 0 #e21861);
+  -webkit-animation: aboutGlow 1s ease-in-out infinite alternate;
+  -moz-animation: aboutGlow 1s ease-in-out infinite alternate;
+  animation: aboutGlow 1s ease-in-out infinite alternate;   
+  -webkit-filter:aboutGlow ease-in-out infinite alternate; 
+}
+#testiPic{
+  
+  -webkit-filter: drop-shadow(2px 2px 0 #e21861)
+                  drop-shadow(-2px -2px 0 #e21861);
+  filter: drop-shadow(2px 2px 0 #e21861) 
+          drop-shadow(-2px -2px 0 #e21861);
+  -webkit-animation: aboutGlow 1s ease-in-out infinite alternate;
+  -moz-animation: aboutGlow 1s ease-in-out infinite alternate;
+  animation: aboutGlow 1s ease-in-out infinite alternate;   
+  -webkit-filter:aboutGlow ease-in-out infinite alternate; 
+}
+#aboutInfoPic{
+  height: 100%;
+  max-height: 730px; 
+  -webkit-filter: drop-shadow(2px 2px 0 #e21861)
+                  drop-shadow(-2px -2px 0 #e21861);
+  filter: drop-shadow(2px 2px 0 #e21861) 
+          drop-shadow(-2px -2px 0 #e21861);
+  -webkit-animation: aboutGlow 1s ease-in-out infinite alternate;
+  -moz-animation: aboutGlow 1s ease-in-out infinite alternate;
+  animation: aboutGlow 1s ease-in-out infinite alternate;   
+  -webkit-filter:aboutGlow ease-in-out infinite alternate; 
+}
+@keyframes aboutGlow {
+  from {
+    -webkit-filter:drop-shadow(0 0 0 #fff, 0 0 5px #fff, 0 0 10px #df0056, 0 0 15px #df0056, 0 0 20px #df0056, 0 0 25px #df0056, 0 0 30px #e60073); 
+    filter: drop-shadow(0 0 0 #fff, 0 0 5px #fff, 0 0 10px #df0056, 0 0 15px #df0056, 0 0 20px #df0056, 0 0 25px #df0056, 0 0 30px #e60073);
+  }
+  to {
+    -webkit-filter:drop-shadow( 0 0 5px #fff, 0 0 10px #ff4da6, 0 0 15px #ff4da6, 0 0 20px #ff4da6, 0 0 25px #ff4da6, 0 0 30px #ff4da6, 0 0 35px #ff4da6);
+    filter: drop-shadow( 0 0 5px #fff, 0 0 10px #ff4da6, 0 0 15px #ff4da6, 0 0 20px #ff4da6, 0 0 25px #ff4da6, 0 0 30px #ff4da6, 0 0 35px #ff4da6);
+  }
+}
+.margCenter{
+  padding: 5%;
+}
+.accMarg{
+  padding-left: 10%;
+  padding-right: 10%;
+}
+.cardWidth{
+width: 90%;
+}
+#cardBlockSize{
+max-width: 20%;
+}
+#cardHeight{
+  max-height: 100%;
+}
+.scrolling-wrapper{
+	overflow-x: auto;
+}
+@media (min-width: 576px) {
+  .card-group.card-group-scroll {
+      overflow-x: auto;
+      flex-wrap: nowrap;
+  }
+}
+.card-group.card-group-scroll > .card {
+  flex-basis: 35%;
+}
+#imgSize{
+  margin: auto;
+  height: 15rem;
+  -webkit-filter: drop-shadow(2px 2px 0 #e21861)
+                  drop-shadow(-2px -2px 0 #e21861);
+  filter: drop-shadow(2px 2px 0 #e21861) 
+          drop-shadow(-2px -2px 0 #e21861);
+  -webkit-animation: aboutGlow 1s ease-in-out infinite alternate;
+  -moz-animation: aboutGlow 1s ease-in-out infinite alternate;
+  animation: aboutGlow 1s ease-in-out infinite alternate;   
+  -webkit-filter:aboutGlow ease-in-out infinite alternate; 
+}
+::-webkit-scrollbar-track
+{
+	background-color: transparent;
+	border-radius: 7.5px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+::-webkit-scrollbar
+{
+	width: 7.5px;
+	background-color: transparent;
+}
+#messageBox{
+  height: 100%;
+}
+input[type="text"]{
+height: 100%;
+}
+#accordHeading{
+  background-color: #f1e9e9;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="bgContainerInd lazy vh-100">
+        <img class="menuIcon fixed-top" src="https://aneeqbass.github.io/hostedImages_portfolio/images/menuIcon.png" alt="menuIcon" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
+
+        <div class="portrait">
+          <img id="glow" class="img-fluid mx-auto rounded-circle border border-3 border-dark d-block w-25" src="https://aneeqbass.github.io/hostedImages_portfolio/images/portrait.jpg" alt="portrait">
+          </div>
+          <div class="paddingCont">
+              <h1 id="title" class="mt-2 text-center">Welcome to Aneeq Bassadien's Digital Portfolio</h1>
+              <h1 id="title" class="mt-2 text-center">Work Hard Play Hard</h1>
+            </div>
+        </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
